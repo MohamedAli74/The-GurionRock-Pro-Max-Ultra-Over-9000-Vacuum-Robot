b72d384c35a21f3866e6bc5680233666c13d47db
MohamedAli74 <mohamedali243.123@gmail.com>
2024-12-25 23:41:54 +0200

    implemented methods

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -15,6 +15,7 @@ public class MessageBusImpl implements MessageBus {
 	ConcurrentHashMap<MicroService , BlockingQueue<Event<?>>> microServicesQueues = new ConcurrentHashMap<>();
 	ConcurrentHashMap<Class<? extends Event<?>>,List<MicroService>> eventSubscribers = new ConcurrentHashMap<>();
 	ConcurrentHashMap<Class<? extends Broadcast>,List<MicroService>> broadCastSubscribers = new ConcurrentHashMap<>();
+	ConcurrentHashMap<Event<?> ,Future<?>> futures = new ConcurrentHashMap<>();
 
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
@@ -28,14 +29,17 @@ public class MessageBusImpl implements MessageBus {
 
 	@Override
 	public <T> void complete(Event<T> e, T result) {
-		// TODO Auto-generated method stub
-
+		Future<T> f = futures.get(e);
+		f.resolve(result);
+		futures.remove(e);
 	}
 
 	@Override
 	public void sendBroadcast(Broadcast b) {
-		// TODO Auto-generated method stub
-
+		List subs = broadCastSubscribers.get(b);
+		for(MicroService m : subs){
+			microServicesQueues.get(m).put(b);
+		}
 	}
 
 	
