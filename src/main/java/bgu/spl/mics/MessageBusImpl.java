9846124241e869dac89879f9429fbd398b980fc4
MohamedAli74 <mohamedali243.123@gmail.com>
2024-12-25 22:38:24 +0200

    init
    init

---
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -0,0 +1,69 @@
+package bgu.spl.mics;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.BlockingQueue;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
+ * Write your implementation here!
+ * Only one public method (in addition to getters which can be public solely for unit testing) may be added to this class
+ * All other methods and members you add the class must be private.
+ */
+public class MessageBusImpl implements MessageBus {
+	ConcurrentHashMap<MicroService , BlockingQueue<Event<?>>> microServicesQueues = new ConcurrentHashMap<>();
+	ConcurrentHashMap<Class<? extends Event<?>>,List<MicroService>> eventSubscribers = new ConcurrentHashMap<>();
+	ConcurrentHashMap<Class<? extends Broadcast>,List<MicroService>> broadCastSubscribers = new ConcurrentHashMap<>();
+
+	@Override
+	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
+		eventSubscribers.get(type);
+	}
+
+	@Override
+	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public <T> void complete(Event<T> e, T result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendBroadcast(Broadcast b) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	@Override
+	public <T> Future<T> sendEvent(Event<T> e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void register(MicroService m) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void unregister(MicroService m) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Message awaitMessage(MicroService m) throws InterruptedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
