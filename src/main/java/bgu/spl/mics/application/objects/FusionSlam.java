6ac4bc3fcbe813bf8afb81b9758b3498f8f586be
MohamedAli74 <mohamedali243.123@gmail.com>
2025-01-02 15:37:35 +0200

    updated the responsibility of the methods in FusionSlam + implementation of FusionSlam/service

--- src/main/java/bgu/spl/mics/application/objects/FusionSlam.java
+++ src/main/java/bgu/spl/mics/application/objects/FusionSlam.java
@@ -1,5 +1,6 @@
 package bgu.spl.mics.application.objects;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
@@ -11,6 +12,62 @@ import java.util.Vector;
 public class FusionSlam {
     private List<LandMark> landMarks;
     private List<Pose> poses;
+
+    public List<LandMark> getLandMarks(){
+        return landMarks;
+    }
+
+    public List<Pose> getPoses(){
+        return poses;
+    }
+
+    public void updatePos(TrackedObject toUpdate,Pose pose){
+        LandMark landMark = searchLandMark(toUpdate.getId());
+        List<CloudPoint> globalCoordinates = fetchGlobalCoordinates(toUpdate,pose);
+        if(landMark == null){
+            landMark = new LandMark(toUpdate.getId()+"",toUpdate.getDescription(),globalCoordinates);
+            landMarks.add(landMark);
+        }else{
+            updateLandMarkCoordinates(landMark,globalCoordinates);
+        }
+    }
+
+    private LandMark searchLandMark(int id) {
+        for(LandMark l : landMarks){
+            if(Integer.parseInt(l.getId()) == id)
+                return l;
+        }
+        return null;
+    }
+
+    private List<CloudPoint> fetchGlobalCoordinates(TrackedObject trackedObject, Pose pose) {
+        List<CloudPoint> preCoordinates = trackedObject.getcoordinates();
+        List<CloudPoint> output = new ArrayList<CloudPoint>();
+        for(CloudPoint cloudPoint : preCoordinates){
+            CloudPoint newGlobalPoint = convertLocalPointToGlobalPoint(cloudPoint,pose);
+            output.add(newGlobalPoint);
+        }
+        return output;
+    }
+
+    private void updateLandMarkCoordinates(LandMark landMark, List<CloudPoint> globalCoordinates) {
+        //TO EDIT!!!
+    }
+
+    private CloudPoint convertLocalPointToGlobalPoint(CloudPoint point, Pose pose) {
+        double xLocal = point.getX();
+        double yLocal = point.getY();
+        double xRobot = pose.getX();
+        double yRobot = pose.getY();
+
+        double yawInRadian = Math.toRadians(pose.getYaw());
+        double cosinYaw = Math.cos(yawInRadian);
+        double sinYaw = Math.sin(yawInRadian);
+        double xGlobal = (cosinYaw * xLocal) - (sinYaw * yLocal) + xRobot;
+        double yGlobal = (sinYaw * xLocal) + (cosinYaw * yLocal) + yRobot;
+
+        return new CloudPoint(xGlobal, yGlobal);
+    }
 
     private FusionSlam(){
         this.landMarks = new Vector<LandMark>();
